8669189d451414bee19172fe73503337eb961dce
icshih <icshih@mac.com>
2020-04-06 23:41:47 +0200

    use the grouped country data.

--- pandemic/web/CountryData.py
+++ pandemic/web/CountryData.py
@@ -1,4 +1,4 @@
-from pandemic.viz.lplot import plot_figure_of, plot_all_figures
+from pandemic.viz.lplot import plot_figure_of, plot_all_figures, plot_figure_country_region
 
 
 class CountryData:
@@ -8,6 +8,12 @@ class CountryData:
 
     def get_provence(self):
         return self.df.columns
+
+    def pick_country_region(self, country_list):
+        return self.df[country_list]
+
+    def get_figure_country_region(self, country_list):
+        return plot_figure_country_region(self.df[country_list])
 
     def get_figure_of(self, province):
         return plot_figure_of(self.df, province)
