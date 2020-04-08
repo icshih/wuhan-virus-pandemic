f6ff9542c7849a28481fd47316cf441a309289c5
icshih <icshih@mac.com>
2020-04-08 15:54:22 +0200

    re-organise the methods used in the application

--- pandemic/web/Tools.py
+++ pandemic/web/Tools.py
@@ -1,14 +1,37 @@
 from pandemic.data.WuhanVirus import CSSETimeSeries as cts
+from pandemic.viz.lplot import plot_confirmed_death_recovered_by
+import pandas as pd
 
 
 class Tools:
 
-    def __init__(self, path):
-        self.df = cts(path)
+    def __init__(self, path_confirmed, path_death, path_recovered):
+        self.path_confirmed = path_confirmed
+        self.path_death = path_death
+        self.path_recovered = path_recovered
+        self.confirmed, self.death, self.recovered = self._load()
+
+    def _load(self):
+        confirmed = cts(self.path_confirmed).get_grouped_country_data()
+        death = cts(self.path_death).get_grouped_country_data()
+        recovered = cts(self.path_recovered).get_grouped_country_data()
+        return confirmed, death, recovered
+
+    def _show_confirmed_only(self, country_list):
+        return self.confirmed.get_figure_country_region(country_list)
+
+    def _show_confirmed_death_recovered(self, country):
+        df = pd.concat([self.confirmed.df[country], self.death.df[country], self.recovered.df[country]], axis=1)
+        df.columns = ["Confirmed", "Death", "Recovered"]
+        return plot_confirmed_death_recovered_by(df)
 
     def create_country_dropdown(self):
+        """
+        Create Country dropdown manu.
+        :return:
+        """
         dropdown = list()
-        countries = self.df.get_country_list()
+        countries = self.confirmed.df.columns
         for n in countries:
             dropdown.append({"label": n, "value": n})
         return dropdown
@@ -17,5 +40,20 @@ class Tools:
         return self.select_countries(list(country))
 
     def select_countries(self, country_list):
-        return self.df.get_grouped_country_data().get_figure_country_region(country_list)
-        # return self.df.merge(country_list).get_all_figures()
+        """
+        Display COVID-19 epidemics in the selected country.
+        :param country_list: One or more countries. If the list length is 1, full COVID-19 data is plotted,
+                             otherwise, the confirmed cases of the selected countries are plotted.
+        :return:
+        """
+        if len(country_list) == 1:
+            return self._show_confirmed_death_recovered(country_list[0])
+        else:
+            return self._show_confirmed_only(country_list)
+
+    def reload(self):
+        """
+        Reload the csv files
+        :return:
+        """
+        self.confirmed, self.death, self.recovered = self.load()
